305d35dc4f927e3d4ba57f6db8fae7b07679d7cd
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-09-17 05:40:23 +0300

    screens created

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
-// Ignore log notification by message
-const Stack = createStackNavigator();
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-function Router() {
+import SignUp from './screens/auth/SignUp'
+import SignIn from './screens/auth/SignIn'
+
+const Stack = createStackNavigator()
+
+const Router = () => {
+  let isSignedIn = false;
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {isSignedIn ?
+          <Stack.Group>
+            <Stack.Screen
+              name="Tabs"
+              component={Tabs}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Chat"
+              component={Chat}
+              options={({ route }) => ({
+                headerStyle: { backgroundColor: '#0088CC' },
+                headerTintColor: 'white',
+                headerTitle: route.params.chatName,
+                headerShadowVisible: false,
+                headerBackVisible: true,
+                headerLeft: () => (
+                  <Image
+                    source={{ uri: route.params.receiver.image }}
+                    style={styles.image}
+                  />
+                ),
+                headerRight: () => (
+                  <Icon name="dots-vertical" size={25} color="white" />
+                ),
+              })}
+            />
+            <Stack.Screen
+              name="Theme"
+              component={Theme}
+              options={({ route }) => ({
+                headerStyle: { backgroundColor: '#0088CC' },
+                headerTintColor: 'white',
+                headerTitle: 'Select Theme',
+                headerShadowVisible: false,
+              })}
+            />
+            <Stack.Screen
+              name="Profile"
+              component={Profile}
+              options={({ route }) => ({
+                headerStyle: { backgroundColor: '#0088CC' },
+                headerTintColor: 'white',
+                headerTitle: 'Edit Profile',
+                headerShadowVisible: false,
+              })}
+            />
+          </Stack.Group>
+          :
+          <Stack.Group screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+          </Stack.Group>
+        }
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
 export default Router
